a663189a2d1cd63b0af19ff6943ab182a873ac1a
edufmass <git@edufmass.ar>
2026-03-01 16:34:07 -0300

    4 lombok logging

--- src/main/java/ar/edufmass/spring7beerapp/controller/BeerController.java
+++ src/main/java/ar/edufmass/spring7beerapp/controller/BeerController.java
@@ -1,12 +1,25 @@
 package ar.edufmass.spring7beerapp.controller;
 
+import ar.edufmass.spring7beerapp.model.Beer;
 import ar.edufmass.spring7beerapp.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
 
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id){
+
+        log.debug("Get Beer by Id - in controller");
+
+        return beerService.getBeerById(id);
+    }
+
 }
--- src/main/java/ar/edufmass/spring7beerapp/services/BeerServiceImpl.java
+++ src/main/java/ar/edufmass/spring7beerapp/services/BeerServiceImpl.java
@@ -2,16 +2,21 @@ package ar.edufmass.spring7beerapp.services;
 
 import ar.edufmass.spring7beerapp.model.Beer;
 import ar.edufmass.spring7beerapp.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by Id - in service. Id: " + id.toString());
+
         return Beer.builder()
                 .id(id)
                 .version(1)
