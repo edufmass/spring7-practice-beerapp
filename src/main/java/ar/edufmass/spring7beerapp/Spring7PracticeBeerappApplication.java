048e1a1591533ed67a44295c8b13263c3cc791e0
edufmass <git@edufmass.ar>
2026-03-01 15:40:45 -0300

    first commit

---
+++ src/main/java/ar/edufmass/spring7beerapp/Spring7PracticeBeerappApplication.java
@@ -0,0 +1,13 @@
+package ar.edufmass.spring7beerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7PracticeBeerappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring7PracticeBeerappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edufmass/spring7beerapp/Spring7PracticeBeerappApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edufmass.spring7beerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7PracticeBeerappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
