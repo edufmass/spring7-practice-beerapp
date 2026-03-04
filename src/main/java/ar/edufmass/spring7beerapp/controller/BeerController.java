4b553243efa02e504ab7ae464a505001611dc862
edufmass <git@edufmass.ar>
2026-03-04 16:07:32 -0300

    10 response header

--- src/main/java/ar/edufmass/spring7beerapp/controller/BeerController.java
+++ src/main/java/ar/edufmass/spring7beerapp/controller/BeerController.java
@@ -4,6 +4,7 @@ import ar.edufmass.spring7beerapp.model.Beer;
 import ar.edufmass.spring7beerapp.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,11 +35,14 @@ public class BeerController {
 
     @PostMapping
     //@RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity handlePost(@RequestBody Beer beer){
+    public ResponseEntity<Void> handlePost(@RequestBody Beer beer){
 
         Beer savedBeer = beerService.saveNewBeer(beer);
 
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
 
 }
