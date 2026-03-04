f389c1482a842913c2e2cbe20da8f81ce7c9448b
edufmass <git@edufmass.ar>
2026-03-04 15:59:05 -0300

    9 create beer

--- src/main/java/ar/edufmass/spring7beerapp/controller/BeerController.java
+++ src/main/java/ar/edufmass/spring7beerapp/controller/BeerController.java
@@ -4,10 +4,9 @@ import ar.edufmass.spring7beerapp.model.Beer;
 import ar.edufmass.spring7beerapp.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -31,6 +30,15 @@ public class BeerController {
         log.debug("Get Beer by Id - in controller");
 
         return beerService.getBeerById(beerId);
+    }
+
+    @PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity handlePost(@RequestBody Beer beer){
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
 }
