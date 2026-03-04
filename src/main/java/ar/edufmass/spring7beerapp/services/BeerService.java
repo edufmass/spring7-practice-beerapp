45073e9fc0269172811b4699960ebbf19508a8f8
edufmass <git@edufmass.ar>
2026-03-04 15:12:44 -0300

    5 list beers

--- src/main/java/ar/edufmass/spring7beerapp/services/BeerService.java
+++ src/main/java/ar/edufmass/spring7beerapp/services/BeerService.java
@@ -2,8 +2,10 @@ package ar.edufmass.spring7beerapp.services;
 
 import ar.edufmass.spring7beerapp.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
     Beer getBeerById(UUID id);
 }
