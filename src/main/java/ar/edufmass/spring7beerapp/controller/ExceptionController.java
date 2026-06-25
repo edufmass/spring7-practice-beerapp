81d7c8c95577cd7ebbbd5590d622bd7760f5d7ca
edufmass <git@edufmass.ar>
2026-06-25 15:31:44 -0300

    37.1 ResposeStatus annotation

--- src/main/java/ar/edufmass/spring7beerapp/controller/ExceptionController.java
+++ src/main/java/ar/edufmass/spring7beerapp/controller/ExceptionController.java
@@ -4,10 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+// With ResponseStatus in NotFoundException, this controller can be deleted
+//@ControllerAdvice
 public class ExceptionController {
 
-    @ExceptionHandler(NotFoundException.class)
+    //@ExceptionHandler(NotFoundException.class)
     public ResponseEntity handleNotFoundException(){
         return ResponseEntity.notFound().build();
     }
